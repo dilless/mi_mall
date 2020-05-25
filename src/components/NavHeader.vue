f299948d047569c2e0fe096c7231e47e786497d6
dilless <bohuang1994@gmail.com>
2020-05-25 23:24:37 +0800

    header

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -9,9 +9,10 @@
           <a href="javascript:">协议规则</a>
         </div>
         <div class="topbar-user">
-          <a href="javascript:">登录</a>
-          <a href="javascript:">注册</a>
-          <a href="javascript:" class="my-cart"><span class="icon-cart"></span>购物车</a>
+          <a href="javascript:" v-if="username">{{ username }}</a>
+          <a href="javascript:" v-else @click="login">登录</a>
+          <a href="javascript:" v-if="username">我的订单</a>
+          <a href="javascript:" class="my-cart" @click="goToCart"><span class="icon-cart"></span>购物车</a>
         </div>
       </div>
     </div>
@@ -23,7 +24,21 @@
         <div class="header-menu">
           <div class="item-menu">
             <span>小米手机</span>
-            <div class="children"></div>
+            <div class="children">
+              <ul>
+                <li class="product" v-for="(item, index) in phoneList" :key="index">
+                  <a :href="'/#/product/' + item.id" target="_blank">
+                    <div class="pro-img">
+                      <img
+                        :src="item.mainImage"
+                        :alt="item.subTitle">
+                    </div>
+                    <div class="pro-name">{{ item.name }}</div>
+                    <div class="pro-price">{{ item.price | currency }}</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="item-menu">
             <span>红米手机</span>
@@ -47,12 +62,47 @@
 
 <script>
 export default {
-  name: 'NavHeader'
+  name: 'NavHeader',
+  data () {
+    return {
+      username: 'jack',
+      phoneList: []
+    }
+  },
+  filters: {
+    currency (val) {
+      if (!val) return '￥0.00元'
+      return '￥' + val.toFixed(2) + '元'
+    }
+  },
+  mounted () {
+    this.getProductList()
+  },
+  methods: {
+    login () {
+      this.$router.push('/login')
+    },
+    getProductList () {
+      this.axios.get('/products', {
+        params: {
+          categoryId: '100012',
+          pageSize: 6
+        }
+      }).then((res) => {
+        this.phoneList = res.list
+      })
+    },
+    goToCart () {
+      this.$router.push('/cart')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/base";
+@import "../assets/scss/mixin";
+@import "../assets/scss/config";
 
 .header {
   .nav-topbar {
@@ -62,9 +112,7 @@ export default {
     color: #B0B0B0;
 
     .container {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+      @include flex();
 
       a {
         display: inline-block;
@@ -79,15 +127,8 @@ export default {
         color: #FFFFFF;
 
         .icon-cart {
-          display: inline-block;
-          width: 16px;
-          height: 12px;
+          @include bgImg(16px, 12px, '/imgs/icon-cart-checked.png');
           margin-right: 4px;
-          background-size: contain;
-        }
-
-        .icon-cart {
-          background: url("/imgs/icon-cart-checked.png") no-repeat center;
         }
       }
     }
@@ -95,10 +136,9 @@ export default {
 
   .nav-header {
     .container {
+      position: relative;
       height: 112px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+      @include flex();
 
       .header-logo {
         display: inline-block;
@@ -113,20 +153,14 @@ export default {
 
           &:before {
             content: ' ';
-            display: inline-block;
-            width: 55px;
-            height: 55px;
-            background: url("/imgs/mi-logo.png") no-repeat center;
-            background-size: contain;
+            @include bgImg(55px, 55px, '/imgs/mi-logo.png');
             transition: margin .2s;
           }
 
           &:after {
             content: ' ';
-            display: inline-block;
-            width: 55px;
-            height: 55px;
-            background: url("/imgs/mi-home.png") no-repeat center;
+            @include bgImg(55px, 55px, '/imgs/mi-home.png');
+
             background-size: contain;
           }
 
@@ -155,6 +189,75 @@ export default {
           }
 
           &:hover {
+            color: $colorA;
+
+            .children {
+              height: 220px;
+              opacity: 1;
+            }
+          }
+
+          .children {
+            position: absolute;
+            top: 112px;
+            left: 0;
+            width: 1226px;
+            height: 0;
+            opacity: 0;
+            overflow: hidden;
+            border-top: 1px solid $colorH;
+            box-shadow: 0 7px 6px 0 rgba(0, 0, 0, 0.11);
+            z-index: 10;
+            transition: all .5s;
+            background-color: #FFFFFF;
+
+            .product {
+              position: relative;
+              float: left;
+              width: 16.6%;
+              height: 220px;
+              font-size: $fontK;
+              line-height: 12px;
+              text-align: center;
+
+              a {
+                display: inline-block;
+              }
+
+              img {
+                height: 111px;
+                margin-top: 26px;
+              }
+
+              .pro-img {
+                height: 137px;
+              }
+
+              .pro-name {
+                font-weight: bold;
+                margin-top: 19px;
+                margin-bottom: 8px;
+                color: $colorB;
+              }
+
+              .pro-price {
+                color: $colorA;
+              }
+
+              &:before {
+                content: ' ';
+                position: absolute;
+                top: 28px;
+                right: 0;
+                border-left: 1px solid $colorF;
+                height: 100px;
+                width: 1px;
+              }
+
+              &:last-child:before {
+                display: none;
+              }
+            }
           }
         }
       }
@@ -178,11 +281,7 @@ export default {
           }
 
           a {
-            display: inline-block;
-            width: 18px;
-            height: 18px;
-            background: url("/imgs/icon-search.png") no-repeat center;
-            background-size: contain;
+            @include bgImg(18px, 18px, '/imgs/icon-search.png');
             margin-left: 17px;
           }
         }
