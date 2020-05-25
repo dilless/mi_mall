62fb84584e133e8e93a4ca0c4c761b9b471114f1
dilless <bohuang1994@gmail.com>
2020-05-25 23:25:13 +0800

    footer

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>NavFooter</div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="" target="_blank">河畔一角主页</a><span>|</span>
+      <a href="" target="_blank">Vue全栈</a><span>|</span>
+      <a href="" target="_blank">React全家桶</a><span>|</span>
+      <a href="" target="_blank">微信支付专项课程</a>
+    </div>
+    <div class="copyright">
+      © 2020 GitHub, Inc. <span class="domain">github.com</span>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -8,6 +23,45 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "../assets/scss/config";
 
+.footer {
+  height: 234px;
+  border-top: 4px solid $colorA;
+  background-color: $colorB;
+  color: $colorD;
+  font-size: $fontI;
+  text-align: center;
+
+  .footer-logo {
+    margin-top: 46px;
+    margin-bottom: 31px;
+
+    img {
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+
+  .footer-link {
+    margin-bottom: 13px;
+
+    a {
+      color: $colorD;
+      display: inline-block;
+    }
+
+    span {
+      margin: 0 10px;
+    }
+  }
+
+  .copyright {
+    .domain {
+      color: $colorA;
+    }
+  }
+}
 </style>
