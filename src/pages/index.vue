45fb6f2921636c4277687f7b3299bcbffe940dc7
dilless <bohuang1994@gmail.com>
2020-05-26 20:44:27 +0800

    phone in index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -66,7 +66,9 @@
           <img src="/imgs/banner-1.png" alt="">
         </a>
       </div>
-      <div class="product-box">
+    </div>
+    <div class="product-box">
+      <div class="container">
         <h2>手机</h2>
         <div class="wrapper">
           <div class="banner-left">
@@ -75,14 +77,15 @@
           <div class="list-box">
             <div class="list" v-for="(arr, i) in phoneList" :key="i">
               <div class="item" v-for="(item, j) in arr" :key="j">
-                <span>新品</span>
+                <span :class="j % 2 === 0 ? 'new-pro' : j % 3 === 0 ? 'kill-pro' : ''">{{ j % 2 === 0 ? '新品' : j % 3 === 0 ? '秒杀' : '' }}</span>
                 <div class="item-img">
-                  <img src="" alt="">
+                  <img :src="item.mainImage"
+                       alt="">
                 </div>
                 <div class="item-info">
-                  <h3>小米9</h3>
-                  <p>晓龙855，索尼4800万超广角</p>
-                  <p>2999元</p>
+                  <h3>{{ item.name }}</h3>
+                  <p>{{ item.subTitle }}</p>
+                  <p class="price">{{ item.price }}元</p>
                 </div>
               </div>
             </div>
@@ -196,10 +199,22 @@ export default {
           img: '/imgs/ads/ads-4.jpg'
         }
       ],
-      phoneList: [
-        [1, 1, 1, 1],
-        [1, 1, 1, 1]
-      ]
+      phoneList: []
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      this.axios.get('/products', {
+        params: {
+          categoryId: 100012,
+          pageSize: 8
+        }
+      }).then((res) => {
+        this.phoneList = [res.list.slice(0, 4), res.list.slice(4, 8)]
+      })
     }
   }
 }
@@ -316,26 +331,101 @@ export default {
       margin-bottom: 50px;
     }
   }
+
   .product-box {
     background-color: $colorJ;
     padding: 30px 0 50px;
+
     h2 {
       font-size: $fontF;
       height: 21px;
       line-height: 21px;
       color: $colorB;
+      margin-bottom: 20px;
     }
+
     .wrapper {
       display: flex;
+
       .banner-left {
         margin-right: 16px;
+
         img {
           width: 224px;
           height: 619px;
         }
       }
-      .list-box {
 
+      .list-box {
+        .list {
+          @include flex();
+          width: 986px;
+          margin-bottom: 14px;
+
+          &:last-child {
+            margin-bottom: 0;
+          }
+
+          .item {
+            width: 236px;
+            height: 302px;
+            background-color: $colorG;
+            text-align: center;
+
+            span {
+              display: inline-block;
+              width: 67px;
+              height: 24px;
+              font-size: 14px;
+              line-height: 24px;
+              color: $colorG;
+
+              &.new-pro {
+                background-color: #7ECF68;
+              }
+
+              &.kill-pro {
+                background-color: #E82626;
+              }
+            }
+
+            .item-img {
+              img {
+                width: 100%;
+                height: 195px;
+              }
+            }
+
+            .item-info {
+              h3 {
+                font-size: $fontJ;
+                color: $colorB;
+                line-height: $fontJ;
+                font-weight: bold;
+              }
+
+              p {
+                color: $colorD;
+                line-height: 13px;
+                margin: 6px auto 13px;
+              }
+
+              .price {
+                color: #F20A0A;
+                font-size: $fontJ;
+                font-weight: bold;
+                cursor: pointer;
+
+                &:after {
+                  @include bgImg(22px, 22px, '/imgs/icon-cart-hover.png');
+                  content: '';
+                  margin-left: 5px;
+                  vertical-align: middle;
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
