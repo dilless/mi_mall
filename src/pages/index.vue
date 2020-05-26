a26e404bc50ec8525b3c6514c866355f705d3746
dilless <bohuang1994@gmail.com>
2020-05-26 15:54:48 +0800

    ads

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -56,8 +56,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/' + item.id" v-for="(item, index) in adsList" :key="index">
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+        <a href="/#/product/30">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -147,6 +155,24 @@ export default {
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
+      ],
+      adsList: [
+        {
+          id: 33,
+          img: '/imgs/ads/ads-1.png'
+        },
+        {
+          id: 48,
+          img: '/imgs/ads/ads-2.jpg'
+        },
+        {
+          id: 45,
+          img: '/imgs/ads/ads-3.png'
+        },
+        {
+          id: 47,
+          img: '/imgs/ads/ads-4.jpg'
+        }
       ]
     }
   }
@@ -247,6 +273,21 @@ export default {
         width: 100%;
         height: 100%;
       }
+    }
+  }
+
+  .ads-box {
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+
+    a {
+      width: 296px;
+      height: 167px;
+    }
+
+    .banner {
+      margin-bottom: 50px;
     }
   }
 }
