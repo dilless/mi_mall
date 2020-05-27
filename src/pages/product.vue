1b0848346ed8fb811cca8a59ddbeb59e221bcd97
dilless <bohuang1994@gmail.com>
2020-05-27 21:53:21 +0800

    product  page

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -40,10 +40,10 @@
         <h2>60帧超慢动作摄影<br/>慢慢回味每一瞬间的精彩</h2>
         <p>后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！<br/>更能AI 精准分析视频内容，15个场景智能匹配背景音效。</p>
         <div class="video-bg" @click="showSlide='slideDown'"></div>
-        <div class="video-box" v-show="showSlide">
-          <div class="overlay"></div>
-          <div class="video" v-bind:class="showSlide">
-            <span class="icon-close" @click="closeVideo"></span>
+        <div class="video-box">
+          <div class="overlay" v-if="showSlide === 'slideDown'"></div>
+          <div class="video" :class="showSlide">
+            <span class="icon-close" @click="showSlide='slideUp'"></span>
             <video src="/imgs/product/video.mp4" muted autoplay controls="controls"></video>
           </div>
         </div>
@@ -65,7 +65,7 @@ export default {
   },
   data () {
     return {
-      showSlide: '1', // 控制动画效果
+      showSlide: '', // 控制动画效果
       product: {}, // 商品信息
       swiperOption: {
         autoplay: true,
@@ -201,14 +201,56 @@ export default {
           z-index: 10;
         }
 
+        @keyframes slideDown {
+          from {
+            top: -50%;
+            opacity: 0;
+          }
+          to {
+            top: 50%;
+            opacity: 1;
+          }
+        }
+
+        @keyframes slideUp {
+          from {
+            top: 50%;
+            opacity: 1;
+          }
+          to {
+            top: -50%;
+            opacity: 0;
+          }
+        }
+
         .video {
           position: fixed;
-          top: 50%;
+          top: -50%;
           left: 50%;
           transform: translate(-50%, -50%);
           width: 1000px;
           height: 536px;
           z-index: 10;
+          opacity: 1;
+
+          &.slideDown {
+            animation: slideDown .6s linear;
+            top: 50%;
+          }
+
+          &.slideUp {
+            animation: slideUp .6s linear;
+          }
+
+          .icon-close {
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            cursor: pointer;
+            z-index: 11;
+            @include bgImg(20px, 20px, '/imgs/icon-close.png')
+          }
+
           video {
             width: 100%;
             height: 100%;
